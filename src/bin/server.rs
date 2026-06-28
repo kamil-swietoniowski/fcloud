c3c4ed5edcd6b95af5eca2d87ae142427a764e5c
Kamil Świętoniowski <kamilswietoniowski91@gmail.com>
2026-06-28 22:43:27 +0200

    refactor: added some comments and stuff

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,4 +1,4 @@
-use axum::{Router, extract::path, routing::{get, post}};
+use axum::{Router, routing::{delete, get, post}};
 
 #[path ="../models.rs"]
 mod models;
@@ -14,11 +14,16 @@ mod file_service;
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/list", get(routers::list_files_handler))
-        .route("/send", post(routers::receive_file_handler));
+        .route("/auth/register", post(routers::handle_register))
+        .route("/auth/login", post(routers::handle_login))
+        .route("/files", get(routers::handle_list_files))
+        .route("/files/init", post(routers::handle_file_init))
+        .route("/files/upload", post(routers::handle_chunk_upload))
+        .route("/files/{id}", get(routers::handle_download_file))
+        .route("/files/{id}", delete(routers::handle_delete_file));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    println!("Serwer fcloud ruszył!");
+    println!("FCloud has started");
     axum::serve(listener, app).await.unwrap();
 }
 
--- src/services/file_service.rs
+++ src/services/file_service.rs
@@ -1,11 +1,14 @@
+// start of upload
 pub fn initialize_upload() {
     todo!()
 }
 
+// mid of upload
 pub fn process_chunk() {
     todo!()
 }
 
+// end of upload
 pub fn finalize_file() {
     todo!()
 }
